10066a8cab4be4f9046153abf3af9906de326572
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2022-08-01 12:58:20 -0300

    Deleting-addressData_userInfoData-fromFirebase

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -5,31 +5,44 @@ import User from "../model/userModel"
 // import error from "../utils/error"
 
 export default class UserController {
-  public static create(req: Request, res: Response, next: NextFunction): void {
+  public static async create(req: Request, res: Response, next: NextFunction): Promise<void> {
     res.send("CREATE")
   }
 
   public static async read(req: Request, res: Response): Promise<void> {
-    const address =
-      await firebase
-        .collection("Address")
-        .doc(req.params.id)
-        .get();
+    try {
+      const address =
+        await firebase
+          .collection("Address")
+          .doc(req.params.id)
+          .get();
 
-    const info =
-      await firebase
-        .collection("Info")
-        .doc(req.params.id)
-        .get();
-            
-    res.status(200).json([address.data(), info.data()])
+      const info =
+        await firebase
+          .collection("Info")
+          .doc(req.params.id)
+          .get();
+
+      res.status(200).json([address.data(), info.data()])
+    } catch (error) {
+      res.status(400).send("Read Error!")
+    }
   }
 
   public static update(req: Request, res: Response, next: NextFunction): void {
     res.send("UPDATE")
   }
 
-  public static delete(req: Request, res: Response, next: NextFunction): void {
-    res.send("DELETE")
+  public static async delete(req: Request, res: Response): Promise<void> {
+    try {
+      await firebase
+        .collection(req.params.col)
+        .doc(req.params.id)
+        .delete();
+
+      res.status(204).end()
+    } catch (error) {
+      res.status(400).send("Delete Error!")
+    }
   }
 }
--- routes/user.ts
+++ routes/user.ts
@@ -6,6 +6,6 @@ const router: Router = Router();
 router.post("/add", userController.create);
 router.get("/:id", userController.read);
 router.put("/edit/:id", userController.update);
-router.delete("/delete/:id", userController.delete);
+router.delete("/delete/:col/:id", userController.delete);
 
 export default router;
