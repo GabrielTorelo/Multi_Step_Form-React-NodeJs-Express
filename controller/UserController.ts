47dfee6ff5fc70757c13e29b33051a0e1329d4fe
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2022-07-31 22:45:53 -0300

    [Hotfix] - Database secret key send by mistake

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -4,19 +4,19 @@ import User from "../model/userModel"
 // import error from "../utils/error"
 
 export default class UserController {
-    public static create(req: Request, res: Response, next: NextFunction): void {
-        res.send("CREATE")
-    }
+  public static create(req: Request, res: Response, next: NextFunction): void {
+    res.send("CREATE")
+  }
 
-    public static read(req: Request, res: Response, next: NextFunction): void {
-        res.send("READ")
-    }
+  public static read(req: Request, res: Response, next: NextFunction): void {
+    res.send("READ")
+  }
 
-    public static update(req: Request, res: Response, next: NextFunction): void {
-        res.send("UPDATE")
-    }
-    
-    public static delete(req: Request, res: Response, next: NextFunction): void {
-        res.send("DELETE")
-    }
+  public static update(req: Request, res: Response, next: NextFunction): void {
+    res.send("UPDATE")
+  }
+
+  public static delete(req: Request, res: Response, next: NextFunction): void {
+    res.send("DELETE")
+  }
 }
