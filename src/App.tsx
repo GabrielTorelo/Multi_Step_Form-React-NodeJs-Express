9fe5a7190c24bb64d9a18d3d96bdc4ed990684d0
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2022-07-31 22:17:20 -0300

    Add-proxyToBackEnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Navigate,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
 } from "react-router-dom"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import Login from "./pages/Login"
 
 const App = () => {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/perfil" element={<Profile />} />
-				<Route path="*" element={<Navigate to="/" />} />
-			</Routes>
-		</Router>
-	)
+  return (
+    <Router>
+    	<Routes>
+    		<Route path="/" element={<Home />} />
+    		<Route path="/login" element={<Login />} />
+    		<Route path="/perfil" element={<Profile />} />
+    		<Route path="*" element={<Navigate to="/" />} />
+    	</Routes>
+    </Router>
+  )
 }
 
 export default App
